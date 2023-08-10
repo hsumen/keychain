720a4e94a9743e1b8f40e496952c3e2ecf127caf
hsu <hsuworking@gmail.com>
2023-08-11 02:09:12 +0800

    init

---
+++ Hsu/Hsu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hsu
+//
+//  Created by hsu tzu Hsuan on 2023/8/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
